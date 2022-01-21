86c09ff719ba6f5925596f5911dadfd96ed70acc
Somlai Ádám <somlaiadam@outlook.hu>
2022-01-21 13:23:32 +0100

    Pinout, LCD library
    Lábak definiálása, LCD_I2C könyvtár behozatal, valamint a soros port inicializálása (teszt célokra).

--- main.ino
+++ main.ino
@@ -1,11 +1,22 @@
 #include <Wire.h>
+#include <LCD_I2C.h>
 
+#define clk 2
+#define dt 3
+#define sw 4
+#define in1 7
+#define in2 8
+#define in3 9
+#define in4 10
+#define in5 11
+#define in6 12
 long weight;
 int pump1ml = 20;
 int pump2ml = 20;
 int pump3ml = 20;
 
 void setup() {
+  Serial.begin(9600);
   lcd.begin();
   pinMode(sw, INPUT_PULLUP);
   pinMode(clk, INPUT);
